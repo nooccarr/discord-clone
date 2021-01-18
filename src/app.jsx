5b3bb492ac35db091647eec4fbd6580d277fca6a
Soohyun Lim <d2axasap@gmail.com>
2021-01-17 22:46:28 -0500

    Added webpack, babel. More dependancies added

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const App = (props) => {
+
+  return (
+    <div>Hello App Component</div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const webpack = require('webpack');
+var path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, './src/index.jsx'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
